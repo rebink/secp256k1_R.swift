d1c9ee4279025d7a874afd728c4b8181598299ea
Mohamed Rebin K <45125106+rebink@users.noreply.github.com>
2022-07-07 11:16:38 +0530

    Rename package

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let dependencies: [Package.Dependency]
 #endif
 
 let package = Package(
-    name: "secp256k1",
+    name: "secp256k1_R",
     products: [
         // WARNING: These APIs should not be considered stable and may change at any time.
         .library(
